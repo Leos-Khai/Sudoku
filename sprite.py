9822a6d20c455b50d78983cc02532f2b85904df2
Khai Shern, Toh <khaishern8235@gmail.com>
2022-06-18 11:22:17 +0800

    Sprite example

--- sprite.py
+++ sprite.py
@@ -5,18 +5,51 @@ class window(arcade.Window):
     def __init__(self):
         super().__init__(800, 800, "Title")
         self.num_sprite = []
+        self.sudo_sprite = [[None for i in range(9)] for j in range(9)]
         self.sl = arcade.SpriteList()
-        for i in range(2, 3):
+        self.generate_num()
+        print(self.num_sprite)
+        # self.sl.append(self.num_sprite[0])
+        self.generate_sprite_list()
+        self.get_image()
+        print(self.num_sprite[0]._texture)
+
+    def generate_num(self):
+        for i in range(0, 9):
             self.num_sprite.append(
                 arcade.create_text_sprite(
-                    str(i), 200+i, 200+i, arcade.color.WHITE, 100, 50
+                    str(i), 200 + i, 200 + i, arcade.color.WHITE, 100, 50
                 )
             )
-            self.sl.append(self.num_sprite[0])
-        print(self.num_sprite)
+
+    def generate_sprite_list(self):
+        start_y = int(800 - 800 / 9)
+        index = 0
+        for x in range(9):
+            start_x = 0
+            for y in range(9):
+                tmpspr = arcade.create_text_sprite(
+                    str(" "),
+                    start_x,
+                    start_y,
+                    arcade.color.WHITE,
+                    50,
+                    int(800 - 800 / 9),
+                )
+                self.sudo_sprite[x][y] = tmpspr
+                self.sl.insert(index, self.sudo_sprite[x][y])
+                start_x += int(800 / 9)
+                index += 1
+            start_y -= int(800 / 9)
+
+    def get_image(self):
+        for i in range(9):
+            for j in range(9):
+                self.sudo_sprite[i][j]._texture = self.num_sprite[0]._texture
+
     def on_draw(self):
         self.sl.draw()
-    
+
 
 def main():
     win = window()
